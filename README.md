ce11e18d68c1bf7f099cd1f96ec9be87e4715ef7
moonburnt <moonburnt@disroot.org>
2021-07-19 17:55:04 +0300

    feat: initial release

---
+++ README.md
@@ -0,0 +1,72 @@
+# LayMix
+
+## Description:
+
+**LayMix** - cli utility to mix image layers into all possible results. It should
+be useful for artists who need to produce few slightly-different versions of same
+image (say, same character but with different hairstyles). Originally I've made
+it for some web visual novel, but then I've got permission to release the sources
+and further extend it.
+
+## Image Requirements:
+
+- All images must have the same (x, y) size
+- All images must be in format that support transparency (png is STRONLY recommended)
+- All images (for now) must follow same naming convention, in order to automatically
+distinguish layers (see "Usage")
+
+## Dependencies:
+
+- Pillow==8.2.0
+
+## Installation:
+
+- `pip install -r requirements.txt`
+
+## Usage:
+
+- Open terminal of your choice in tool's directory
+- `laymix_cli --prefixes first_layer second_layer third_layer` (and so on)
+Where "{number}_layer" is either full name of layer or part of it.
+You can optionally pass `path_to_dir_with_images` before --prefixes - then tool
+will look for specific place where your files are located. Else it attempts to
+search for images in "./images" directory.
+Passing `--savedir path_to_dir` will set custom directory to save images into.
+Default is "./results"
+
+Say, you have image with character's head drawn on it. And you want final images
+to have all custom hairstyles and facial expessions you've made. For this task,
+you will need (aside from base image) 2 image groups: one for hairs, other for
+facial expressions.
+You save these layers as separate images and name them like that:
+`{background}_{name_of_layer}_{number}.png` (without brakes)
+In case of our example, your images directory should look like that:
+`lilu.png`
+`lilu_face_1.png`
+`lilu_face_2.png`
+`lilu_face_3.png`
+`lilu_hair_1.png`
+`lilu_hair_2.png`
+and so on.
+
+Then you run tool like `./laymix_cli --prefixes face hair`
+If everything has been done correctly - you will get 6 different images in
+"results" directory.
+But what if original image has already included some hair or facial expression?
+Just add `--include-background` after command above - and you will keep them all
+(and amount of generated images will (in case of our example) increase to 11)
+
+## Limitations:
+
+- For the time being, this tool is a bit ram-heavy. Providing many images/layers
+at once may cause your machine to dive into swap
+- The only supported image formats are those, pillow can understand. E.g you will
+have to export your ".sai"/".psd" to something like ".png". Im sorry
+
+## License:
+
+[GPLv3](LICENSE)
+
+This **only affects this utility itself and not projects you use it in** - you
+are free to produce pics under whatever license you want and use them in whatever
+projects you want, will they be FOSS or proprietary
---
+++ laymix/laymix.py
@@ -0,0 +1,185 @@
+## laymix - utility to mix image layers into all possible results
+## Copyright (c) 2021 moonburnt
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0.txt
+
+from os import listdir, makedirs
+from os.path import isfile, isdir, join, splitext, basename, dirname
+from PIL import Image
+from itertools import product
+from dataclasses import dataclass
+import logging
+
+log = logging.getLogger(__name__)
+
+@dataclass
+class ImageParts:
+    name: str
+    image: str
+    parts: dict
+
+class LayerMixer:
+    def __init__(self, prefixes:list, savedir:str):
+        self.prefixes = prefixes
+        self.savedir = savedir
+        makedirs(self.savedir, exist_ok = True)
+
+    def get_files(self, pathtodir:str) -> list:
+        """Get list of files in directory"""
+        files = []
+        if isfile(pathtodir):
+            log.debug(f"{pathtodir} itself is a file, returning")
+            files.append(pathtodir)
+            return files
+
+        log.debug(f"Attempting to parse directory {pathtodir}")
+        directory_content = listdir(pathtodir)
+        log.debug(f"Uncategorized content inside is: {directory_content}")
+
+        for item in directory_content:
+            log.debug(f"Processing {item}")
+            itempath = join(pathtodir, item)
+            if isdir(itempath):
+                log.debug(f"{itempath} leads to directory, processing its content")
+                #looping over this very function for all subdirectories
+                files += self.get_files(itempath)
+            else:
+                #assuming that everything that isnt directory is file
+                log.debug(f"{itempath} leads to file, adding to list")
+                files.append(itempath)
+
+        log.debug(f"Got following files in total: {files}")
+        return files
+
+    def filter_by_mask(self, files:list, mask:str,
+                        exact_match:bool = False) -> list:
+        """Get items that has provided mask in them"""
+        filtered = []
+
+        for item in files:
+            #TODO: maybe make case-insensetivity an option
+            item_name = str(item).lower()
+            mask = mask.lower()
+            if exact_match:
+                if (item_name == mask) or (splitext(item_name)[0] == mask):
+                    filtered.append(item)
+                continue
+            if mask in item_name:
+                filtered.append(item)
+
+        log.debug(f"Got following items matching mask {mask}: {filtered}")
+        return filtered
+
+    def make_constructors(self, files:list,
+                                include_background:bool = False) -> ImageParts:
+        """Create image constructors to use with self.build_images()"""
+        raw_items = {}
+        for prefix in self.prefixes:
+            items = self.filter_by_mask(
+                                        files = files,
+                                        mask = prefix,
+                                        )
+            raw_items[prefix] = items
+
+        backgrounds = []
+        for f in files:
+            exists = False
+
+            for part in raw_items:
+                if f in raw_items[part]:
+                    exists = True
+                    break
+
+            if not exists:
+                log.debug(f"Threating {f} as background")
+                backgrounds.append(f)
+
+        constructors = []
+        for item in backgrounds:
+            pic_name = basename(splitext(item)[0])
+            pic_parts = {}
+            valid_parts_counter = 0
+            for part in raw_items:
+                items = self.filter_by_mask(
+                                            files = raw_items[part],
+                                            mask = pic_name,
+                                            )
+                pic_parts[part] = items
+                if items:
+                    valid_parts_counter += 1
+
+            if not valid_parts_counter:
+                log.warning(f"{item} doesnt seem to have any layers, skipping")
+                continue
+
+            if include_background:
+                for part in pic_parts:
+                    pic_parts[part].append(None)
+
+            image_constructor = ImageParts(
+                                        name = pic_name,
+                                        image = item,
+                                        parts = pic_parts,
+                                        )
+
+            constructors.append(image_constructor)
+
+        log.debug(f"Got following image constructors: {constructors}")
+        return constructors
+
+    def build_images(self, constructor:ImageParts) -> list:
+        """Build all possible image variants out of provided constructor"""
+        log.debug(f"Building {constructor.name}")
+
+        def add_image_layer(image, layer):
+            img = Image.alpha_composite(image, layer)
+            return img
+
+        images = []
+
+        background = Image.open(constructor.image)
+
+        img_parts = []
+        for part in constructor.parts:
+            #fixing issue with product returning [] on non-existing lists
+            if constructor.parts[part]:
+                img_parts.append(constructor.parts[part])
+
+        #getting all possible parts variations:
+        variations = list(product(*img_parts))
+
+        for sequence in variations:
+            layered_img = background
+            for path in sequence:
+                if not path:
+                    continue
+
+                layer = Image.open(path)
+                layered_img = add_image_layer(layered_img, layer)
+
+            images.append(layered_img)
+
+        log.debug(f"Got {len(images)} images total")
+
+        return images
+
+    def save(self, images:list, name_mask:str, close_after_done:bool = True):
+        """Save specified images on disk"""
+        for number, item in enumerate(images):
+            filepath = join(self.savedir, f"{name_mask}_{number}")
+            filename = f"{filepath}.png"
+            item.save(filename)
+            log.debug(f"Successfully saved {filename} to disk")
+            if close_after_done:
+                item.close()
